91fb03853670178a133c49d50f7843bd957c3c16
Alexander Urizar <urizaraj@gmail.com>
2018-04-04 16:04:46 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,19 @@
 class GamesController < ApplicationController
   def create
-
+    render json: Game.new
   end
 
   def show
-
+    game = Game.find(params[:id])
+    render json: game
   end
 
   def update
-
+    game = Game.find(params[:id])
+    game.update(state: params[:state])
+    render json: game
   end
 
   def index
-
   end
 end
