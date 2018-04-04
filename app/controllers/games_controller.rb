127fc8361361ee59ff50e5f37198489298308f9c
Alexander Urizar <urizaraj@gmail.com>
2018-04-04 15:48:12 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,17 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  def create
+    
+  end
 
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def index
+    
+  end
 end
